dd5f16c05bf27d977813b4dc630a6f13d8140f31
Miguel Aguila <maguila.apple@gmail.com>
2022-01-07 12:20:20 +0800

    Completed problem function in playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,24 +1,17 @@
 import UIKit
-func xMaxArray(x: Int) ->[Int]{
-    var xArray = [Int]()
-    var counter = 0
-    var xMax = x
-    if x % 2 == 0{
-        xMax = x - 1
-        while xMax > (-1){
-            xArray[counter] = xMax
-            xMax = xMax - 2
-            counter = counter + 1
+
+func getArray(WithMax max: Int) -> [Int]{
+    var oddElements = [Int]()
+    oddElements.append(0)
+    for currentNumber in 0 ... max{
+        if currentNumber % 2 == 1{
+            oddElements.append(currentNumber)
         }
     }
-    else{
-        while xMax > (-1){
-            xArray[counter] = xMax
-            xMax = xMax - 2
-            counter = counter + 1
-        }
-    }
-    return xArray
+return oddElements
 }
+
+getArray(WithMax: 10)
+
 
 
